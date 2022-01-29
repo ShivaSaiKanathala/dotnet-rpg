a86cbd1e71ee53b777a6254c88edf29934c96b3a
Shiva Skathathala <Shiva.Skathathala@grote.com>
2022-01-29 14:31:58 -0500

    Added Login API

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -29,5 +29,17 @@ namespace dotnet_rpg.Controllers
             }
             return Ok(response);
         }
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDTO request)
+        {
+            var response = await _authRepo.Login(
+               request.UserName, request.Password
+            );
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
     }
 }
---
+++ DTO/User/UserLoginDTO.cs
@@ -0,0 +1,8 @@
+namespace dotnet_rpg.DTO.User
+{
+    public class UserLoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
