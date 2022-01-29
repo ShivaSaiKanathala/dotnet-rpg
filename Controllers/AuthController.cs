c857eead32f892aeb2f22eedae0b2907b269f0c1
Shiva Skathathala <Shiva.Skathathala@grote.com>
2022-01-29 14:01:35 -0500

    Added registermethod and conf service startup

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Data;
+using dotnet_rpg.DTO.User;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDTO request)
+        {
+            //added user object to add any additonal information like dob,gender
+            var response = await _authRepo.Register(
+                new User { Username = request.Username }, request.Password
+            );
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
---
+++ DTO/User/UserRegisterDTO.cs
@@ -0,0 +1,8 @@
+namespace dotnet_rpg.DTO.User
+{
+    public class UserRegisterDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
